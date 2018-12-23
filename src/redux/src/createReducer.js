8c2a31cc684ec88859dcaa83ca3cfe931f84e0bf
Arutyunyan Artyom <artalar@ya.ru>
2018-12-23 00:20:48 +0000

    feat(redux): full-compatible createStore implementation

--- src/redux/src/createReducer.js
+++ src/redux/src/createReducer.js
@@ -1,51 +1,53 @@
 // import ActionTypes from './utils/actionTypes';
 
-const IS_REDUCER = '@@UPS/redux/reducer/IS_REDUCER';
-const PREFIX = '@@UPS/redux/reducer/PREFIX';
-const PRIORITY_LEVEL = '@@UPS/redux/reducer/PRIORITY_LEVEL';
-let reducersCount = 0;
+const IS_REDUCER = '@@UPS/redux/reducer/IS_REDUCER'
+const PREFIX = '@@UPS/redux/reducer/PREFIX'
+const PRIORITY_LEVEL = '@@UPS/redux/reducer/PRIORITY_LEVEL'
+let reducersCount = 0
 function noop() {}
 
-export function createReducer(initialState) {
-  const type = `${PREFIX}/${reducersCount++}`;
-  const handlers = {};
+export default function createReducer(initialState) {
+  const type = `${PREFIX}/${reducersCount++}`
+  const handlers = {}
 
   function reducer(state = initialState, action, dispatch = noop) {
     if (handlers.hasOwnProperty(action.type)) {
-      const payload = action.type.startsWith(PREFIX) ? action.state : action;
-      const newState = handlers[action.type](state, payload);
+      const payload = action.type.startsWith(PREFIX) ? action.state : action
+      const newState = handlers[action.type](state, payload)
       if (newState !== state) {
-        dispatch(type, newState);
-        return newState;
+        dispatch(type, newState)
+        return newState
       }
       // else return state
     }
-    return state;
+    return state
   }
 
-  reducer.type = type;
-  reducer[IS_REDUCER] = true;
-  reducer[PRIORITY_LEVEL] = 0;
-
+  reducer.type = type
+  reducer[IS_REDUCER] = true
+  reducer[PRIORITY_LEVEL] = 0
 
   reducer.on = function on(target, handler) {
-    const actionType = typeof target === 'string' ? target : target.type;
+    const actionType = typeof target === 'string' ? target : target.type
 
     if (actionType === undefined) {
-      throw new Error('Can not resolve action type of targer');
+      throw new Error('Can not resolve action type of targer')
     }
     if (handlers.hasOwnProperty(actionType)) {
-      throw new Error(`Action type "${actionType}" already binded to reducer`);
+      throw new Error(`Action type "${actionType}" already binded to reducer`)
     }
 
-    handlers[actionType] = handler;
+    handlers[actionType] = handler
 
     if (target[IS_REDUCER]) {
-      reducer[PRIORITY_LEVEL] = Math.max(reducer[PRIORITY_LEVEL], target[PRIORITY_LEVEL] + 1);
+      reducer[PRIORITY_LEVEL] = Math.max(
+        reducer[PRIORITY_LEVEL],
+        target[PRIORITY_LEVEL] + 1
+      )
     }
 
-    return reducer;
-  };
+    return reducer
+  }
 
-  return reducer;
+  return reducer
 }
--- src/redux/src/createStore.js
+++ src/redux/src/createStore.js
@@ -1,60 +1,138 @@
-import $$observable from 'symbol-observable';
-import { PubSub } from '../../../es';
-import ActionTypes from './utils/actionTypes';
+import $$observable from 'symbol-observable'
+import { PubSub } from '@artalar/ups'
 
-const REDUCER_EVENT_TYPE = '@@UPS/redux/REDUCER_EVENT_TYPE';
+import ActionTypes from './utils/actionTypes'
+import isPlainObject from './utils/isPlainObject'
 
-export function createStore(reducer, preloadedState, enhancer) {
+const REDUCER_EVENT_TYPE = '@@UPS/redux/REDUCER_EVENT_TYPE'
+
+function sybchronizeDispatch(pubSub) {
+  const dispatchOriginal = pubSub.dispatch.bind(pubSub)
+  pubSub.dispatch = function dispatch(eventType, payload) {
+    if (eventType === REDUCER_EVENT_TYPE) this._isPublishing = false
+    dispatchOriginal(eventType, payload)
+  }.bind(pubSub)
+}
+
+export default function createStore(reducer, preloadedState, enhancer) {
+  if (
+    (typeof preloadedState === 'function' && typeof enhancer === 'function') ||
+    (typeof enhancer === 'function' && typeof arguments[3] === 'function')
+  ) {
+    throw new Error(
+      'It looks like you are passing several store enhancers to ' +
+        'createStore(). This is not supported. Instead, compose them ' +
+        'together to a single function'
+    )
+  }
+
   if (typeof preloadedState === 'function' && typeof enhancer === 'undefined') {
-    enhancer = preloadedState;
-    preloadedState = undefined;
+    enhancer = preloadedState
+    preloadedState = undefined
   }
 
   if (typeof enhancer !== 'undefined') {
-    return enhancer(createStore)(reducer, preloadedState);
+    if (typeof enhancer !== 'function') {
+      throw new Error('Expected the enhancer to be a function.')
+    }
+
+    return enhancer(createStore)(reducer, preloadedState)
   }
 
+  if (typeof reducer !== 'function') {
+    throw new Error('Expected the reducer to be a function.')
+  }
 
-  const pubSub = new PubSub();
-  // let isFinalQueueEmpty = false;
-  // const dispatchOriginal = pubSub.dispatch;
-  // const _startPublishOriginal = pubSub._startPublish;
-  // pubSub.dispatch = function(eventType, payload) {
-  //   isFinalQueueEmpty = this._finalQueue.size === 0;
-  //   dispatchOriginal.call(pubSub, eventType, payload)
-  // }.bind(pubSub);
-  // pubSub._startPublish = function() {
-  //   this._isPublishing = !this._isPublishing || (this._isPublishing && isFinalQueueEmpty && !this._finalQueue.size) ? false : true;
-  //   isFinalQueueEmpty = false;
-  //   _startPublishOriginal.call(pubSub)
-  // }.bind(pubSub);
+  const pubSub = new PubSub()
+  sybchronizeDispatch(pubSub)
 
   const pubSubDispatch = pubSub.dispatch.bind(pubSub)
 
-  let currentReducer = reducer;
-  let state = preloadedState;
+  let currentReducer = reducer
+  let state = preloadedState
+  let isDispatching = false
 
   function update(action) {
-    state = currentReducer(state, action, pubSubDispatch);
+    isDispatching = true
+    try {
+      state = currentReducer(state, action, pubSubDispatch)
+    } finally {
+      isDispatching = false
+    }
   }
 
   function getState() {
-    return state;
+    if (isDispatching) {
+      throw new Error(
+        'You may not call store.getState() while the reducer is executing. ' +
+          'The reducer has already received the state as an argument. ' +
+          'Pass it down from the top reducer instead of reading it from the store.'
+      )
+    }
+
+    return state
   }
 
   function subscribe(listener, eventType = REDUCER_EVENT_TYPE) {
+    if (typeof listener !== 'function') {
+      throw new Error('Expected the listener to be a function.')
+    }
+
+    if (isDispatching) {
+      throw new Error(
+        'You may not call store.subscribe() while the reducer is executing. ' +
+          'If you would like to be notified after the store has been updated, subscribe from a ' +
+          'component and invoke store.getState() in the callback to access the latest state. ' +
+          'See https://redux.js.org/api-reference/store#subscribe(listener) for more details.'
+      )
+    }
+
     const cb = eventType === REDUCER_EVENT_TYPE ? () => listener() : listener
-    return pubSub.subscribe(cb, eventType);
+    const _unsubscribe = pubSub.subscribe(cb, eventType)
+
+    return function unsubscribe() {
+      if (isDispatching) {
+        throw new Error(
+          'You may not unsubscribe from a store listener while the reducer is executing. ' +
+            'See https://redux.js.org/api-reference/store#subscribe(listener) for more details.'
+        )
+      }
+
+      _unsubscribe()
+    }
   }
 
   function dispatch(action) {
-    pubSubDispatch(REDUCER_EVENT_TYPE, action);
+    if (!isPlainObject(action)) {
+      throw new Error(
+        'Actions must be plain objects. ' +
+          'Use custom middleware for async actions.'
+      )
+    }
 
-    return action;
+    if (typeof action.type === 'undefined') {
+      throw new Error(
+        'Actions may not have an undefined "type" property. ' +
+          'Have you misspelled a constant?'
+      )
+    }
+
+    if (isDispatching) {
+      throw new Error('Reducers may not dispatch actions.')
+    }
+
+    pubSubDispatch(REDUCER_EVENT_TYPE, action)
+
+    return action
   }
 
   function replaceReducer(nextReducer) {
+    if (typeof nextReducer !== 'function') {
+      throw new Error('Expected the nextReducer to be a function.')
+    }
+
     currentReducer = nextReducer
+    dispatch({ type: ActionTypes.REPLACE })
   }
 
   function observable() {
@@ -82,7 +160,7 @@ export function createStore(reducer, preloadedState, enhancer) {
     }
   }
 
-  pubSub.subscribe(update, REDUCER_EVENT_TYPE, 0);
+  pubSub.subscribe(update, REDUCER_EVENT_TYPE, 0)
 
   dispatch({ type: ActionTypes.INIT })
 
